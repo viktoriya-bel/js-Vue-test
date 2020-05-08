03ffafccf3bbf8b9c3f1fc11c6ee8b160cefa894
vb@finfort.ru <vb@finfort.ru>
2020-05-08 17:38:56 +0400

    получение с апи и вывод

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,83 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <b-form-input v-model="textSearch" placeholder="Поиск"></b-form-input>
+      <div class="mt-2">Value: {{ textSearch }}</div>
+    </div>
+    <div>
+      <b-table small :fields="fields" :items="$parent.users | searchFilter" responsive="sm"  show-empty>
+        <div> </div>
+        <template v-slot:cell(index)="date">
+          {{  date.index + 1 }}
+        </template>
+
+        <template v-slot:cell(name-first)="date">
+          {{ date.item.name.first }}
+        </template>
+
+        <template v-slot:cell(name-last)="date">
+          {{ date.item.name.last }}
+        </template>
+
+        <template v-slot:cell(dob)="date">
+          {{ new Date(date.item.dob.date) }}
+        </template>
+
+        <template v-slot:cell(link-foto)="date">
+          <b-avatar v-bind:src="date.item.picture.thumbnail" v-if="date.item.picture.thumbnail != undefined"></b-avatar>
+          <b-avatar variant="secondary" v-else></b-avatar>
+        </template>
+
+      </b-table>
+    </div>
+<!--    <div>{{ $parent.users }}</div>-->
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+    return {
+      fields: [
+        { key: 'index', label: '№' },
+        { key: 'name-first', label: 'Имя' },
+        { key: 'name-last', label: 'Фамилия' },
+        { key: 'dob', label: 'Дата рождения'},
+        { key: 'link-foto', label: 'Фото'},
+      ],
+      textSearch: ''
+    }
+  },
+  props:{
+    users: {
+      type: String
+    }
+  },
+  filters:{
+    searchFilter(value){
+      let tex = this.textSearch
+      console.log(tex)
+      console.log(value)
+    }
   }
+  // computed:{
+  //   users: function(){
+  //     var comp = this.company;
+  //     return this.phones.filter(function (elem) {
+  //
+  //       if(comp==='') return true;
+  //       else return elem.company.indexOf(comp) > -1;
+  //     })
+  //   }
+  // }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import Vue from 'vue'
 import App from './App.vue'
+import * as axios from "axios"
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 new Vue({
   render: h => h(App),
+  data: {
+    users: []
+  },
+  created() {
+    var vm = this
+    this.$http
+    .get('https://randomuser.me/api/?results=25')
+    .then(function (response) {
+      vm.users = response.data;
+      vm.users = vm.users.results
+      // console.log(vm.users)
+    });
+  }
 }).$mount('#app')
